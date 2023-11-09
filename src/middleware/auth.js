6e982ab4124c18b4e9955ee44c49177f86111193
trevorregister <trevor.register@gmail.com>
2023-11-09 16:26:33 -0500

    feat: auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+const { HttpError } = require('../config/errors')
+
+module.exports = function (req, res, next) {
+    try {
+        const token = req.cookies.authcookie
+        if (!token) { throw new HttpError(401, 'not logged in') }
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET)
+        if(!verified) { throw new HttpError(403, 'unauthorized') }
+        console.log(verified)
+        next()
+
+    }
+    catch (err){
+        next(err)
+    }
+}
