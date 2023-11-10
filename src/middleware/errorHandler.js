80410bff52c4d12e0d1a33d1c6a84bcd4ced79cc
trevorregister <trevor.register@gmail.com>
2023-11-10 10:51:20 -0500

    feat: better error logging

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,4 @@
 module.exports = function returnError (err, req, res, next) {
-    console.log(err.code || 500, err.message)
-    res.status(err.code || 500).send(err.message)
+    console.log(err.code || 500, err)
+    res.status(err.code || 500).send(err)
    }
