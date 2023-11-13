e5c09c8cadc02ad72979d863bc191917904111cc
trevorregister <trevor.register@gmail.com>
2023-11-12 22:19:39 -0500

    chore: removing comments

--- src/modules/events/use-cases/AddEvent.js
+++ src/modules/events/use-cases/AddEvent.js
@@ -3,7 +3,6 @@ const Event = require('../model')
 
 module.exports = (repository) => {
     async function execute(name, description, start, end, capacity){
-        console.log("🚀 ~ file: AddEvent.js:9 ~ execute ~ start, end:", start, end)
         const event = new Event(name, description, start, end, capacity)
         return await repository.create(event)
 
