a4e67f290fff5f220626e1112d6603bdf290ebb3
trevorregister <trevor.register@gmail.com>
2023-11-12 22:19:17 -0500

    refactor: converting to date objects

--- src/modules/shifts/use-cases/AddShift.js
+++ src/modules/shifts/use-cases/AddShift.js
@@ -10,7 +10,10 @@ module.exports = (repository) => {
     async function execute(name, description, teamId, start, end, capacity){
         const team = await teamService.getTeamById(teamId)
         if (!team) {throw new HttpError(404, `team ${teamId} not found`)}
-        const shift = new Shift(name, description, teamId, start, end, capacity)
+        start = new Date(start)
+        end = new Date(end)
+        const duration = (end - start)/3600000 //converts to hours
+        const shift = new Shift(name, description, teamId, start, end, duration, capacity)
         return await repository.create(shift)
 
     }
