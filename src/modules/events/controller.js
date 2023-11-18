7aa68ffa586f38dcff7a69dedd8526420c6cdf25
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2023-11-17 20:21:19 -0500

    feature: event dto

--- src/modules/events/controller.js
+++ src/modules/events/controller.js
@@ -1,14 +1,14 @@
 const { AddEvent, UpdateEvent, GetEventById } = require('./use-cases/_index')
 const { HttpError } = require('../../config/errors')
+const EventDTO = require('./dto')
 
 module.exports = (repository) => {
 
     const addEvent = async (req, res, next) => {
         try {
             const addEventCase = AddEvent(repository)
-            const { name, description, start, end, capacity } = req.body
-            const newEvent = await addEventCase.execute(name, description, start, end, capacity)
-            res.status(201).send(newEvent)
+            const newEvent = await addEventCase.execute(EventDTO.toDb(req.body))
+            res.status(201).send(EventDTO.toWeb(newEvent))
         } catch (err) {
             next(err)
         }
@@ -17,12 +17,11 @@ module.exports = (repository) => {
     const getEventById = async (req, res, next) => {
         try {
             const getEventByIdCase = GetEventById(repository)
-            const { id } = req.params
-            const event = await getEventByIdCase.execute(id)
+            const event = await getEventByIdCase.execute(req.params.id)
             if(!event){
                 throw new HttpError(404, `${id} event not found`)
             }
-            res.status(200).send(event)
+            res.status(200).send(EventDTO.toWeb(event))
         } catch (err) {
             next(err)
         }
