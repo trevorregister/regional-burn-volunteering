90d52fabcdfa49413781cb018657249d592e3e08
trevorregister <trevor.register@gmail.com>
2024-02-12 10:38:36 -0500

    refactor: services use repos instead of use cases

--- src/modules/services.js
+++ src/modules/services.js
@@ -7,8 +7,6 @@ const ShiftRepository = require('./shifts/repository')
 const EventDatabase = require('./events/data_access/database')
 const EventRepository = require('./events/repository')
 const { GetTeamById, AddTeam, UpdateTeam } = require('./teams/use-cases/_index')
-const { GetUserById, LoginUser, GetUsers } = require('./users/use-cases/_index')
-const { GetShiftById, AddShift, UpdateShift } = require('./shifts/use-cases/_index')
 const { GetEventById, AddEvent, UpdateEvent } = require('./events/use-cases/_index')
 
 const teamDatabase = new TeamDatabase()
@@ -33,37 +31,18 @@ class TeamService extends Service {
     }
 
     getTeamById(id) {
-        return GetTeamById(this.repository).execute(id)
-    }
-
-    addTeam(name, description){
-        return AddTeam(this.repository).execute(name, description)
-    }
-
-    updateTeam(id, update){
-        return UpdateTeam(this.repository).execute(id, update)
+        return this.repository.getTeamById(id)
     }
 }
 
 class UserService extends Service {
-    constructor(){
+    constructor(repository){
+        super(repository)
         this.repository = userRepository 
     }
 
     getUserById(id){
-        return GetUserById(this.repository).execute(id)
-    }
-
-    addUser(name, email, role, password){
-        return AddUser(this.repository).execute(name, email, role, password)
-    }
-
-    loginUser(email, password){
-        return LoginUser(this.repository).execute(email, password)
-    }
-
-    getUsers(){
-        return GetUsers(this.repository).execute()
+        return this.repository.getUserById(id)
     }
 }
 
@@ -73,15 +52,7 @@ class ShiftService extends Service{
     }
 
     getShiftById(id){
-        return GetShiftById(this.repository).execute(id)
-    }
-
-    addShift(name, description, teamId, start, end, capacity){
-        return AddShift(this.repository).execute(name, description, teamId, start, end, capacity)
-    }
-
-    updateShift(id, update){
-        return UpdateShift(this.repository).execute(id, update)
+        return this.repository.getShiftById(id)
     }
 }
 
@@ -92,14 +63,6 @@ class EventService extends Service{
 
     getEventById(id){
         return GetEventById(this.repository).execute(id)
-    }
-
-    addEvent(name, description, start, end, capacity){
-        return AddEvent(this.repository).execute(name, description, start, end, capacity)
-    }
-
-    updateEvent(id, update){
-        return UpdateEvent(this.repository).execute(id, update)
     }
 
 }
