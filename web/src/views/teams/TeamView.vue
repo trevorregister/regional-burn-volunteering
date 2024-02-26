2018de5d79d2b24997f05ff7d6bc8c04eae93d8b
trevorregister <trevor.register@gmail.com>
2024-02-26 17:00:23 -0500

    feat: team view first commit

---
+++ web/src/views/teams/TeamView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        {{ team }}
+    </div>
+</template>
+<script>
+import { client } from '../../../api-client/client'
+
+export default {
+    props: ['id'],
+    data() {
+        return {
+            team: {},
+        }
+    },
+    methods: {
+        async getTeamById(id){
+            const team = await client.teams.getTeamById(id)
+            this.team = team.data
+        }
+    },
+    async created() {
+        this.getTeamById(this.id)
+    }
+}
+</script>
+<style>
+    
+</style>
