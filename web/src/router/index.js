d9644b0a77e1f1aea4afbb6a67867d7e453eb7f6
trevorregister <trevor.register@gmail.com>
2024-02-26 08:49:02 -0500

    feat: login view first commit

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import UserCard from '../views/UserCard.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: UserCard
+      component: LoginView
     },
     {
       path: '/about',
---
+++ web/src/views/LoginView.vue
@@ -0,0 +1,51 @@
+
+
+<template>
+  <v-sheet width="300" elevation="2">
+    <v-form @submit.prevent="login">
+      <v-text-field
+        v-model="email"
+        label="Email"
+      >
+      </v-text-field>
+      <v-text-field
+        v-model="password"
+        label="Password"
+      >
+    </v-text-field>
+    <v-btn type="submit">
+      Submit
+    </v-btn>
+    </v-form>
+    <p v-for="(value, key) in user" :key="key">
+    {{ key }}:  {{ value }}
+  </p>
+  </v-sheet>
+</template>
+
+<script>
+import { client } from '../../api-client/client'
+
+export default {
+  name: 'LoginView',
+  data() {
+    return {
+      email: 'user45@email.com',
+      password: 'password',
+      user: {}
+    }
+  },
+  methods: {
+    async login(){
+      const user = await client.users.login({
+        email: this.email,
+        password: this.password
+      })
+      this.user = user.data
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
