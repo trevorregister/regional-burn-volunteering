d7729d14131f44d6bc8c2d02fcb820c8949329d5
trevorregister <trevor.register@gmail.com>
2024-02-27 10:18:53 -0500

    feat: dashboard using userStore

--- web/src/views/users/DashboardView.vue
+++ web/src/views/users/DashboardView.vue
@@ -1,10 +1,38 @@
 <template>
     <div>
         <h1>Dashboard</h1>
+        {{ user }}
     </div>
 </template>
 <script>
+import { initUserStore } from '../../stores/user'
+import { client } from '@client'
 export default {
-    
+    data() {
+        return {
+            user: {},
+            userStore: initUserStore()
+        }
+    },
+    methods: {
+        async load() {
+            if(this.userId === ''){
+                this.$router.push({path: '/login'})
+            }
+            const userResponse = await client.users.getUserById(this.userId)
+            this.user = userResponse.data
+        }
+    },
+    computed: {
+        userId() {
+            return this.userStore.userId
+        },
+        token() {
+            return this.userStore.token
+        },
+    },
+    async created(){
+        await this.load()
+    }
 }
 </script>
