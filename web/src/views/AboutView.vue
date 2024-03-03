4f0a2fe7acc848e2affca0054219e41203f34d96
trevorregister <trevor.register@gmail.com>
2024-03-03 11:52:13 -0500

    refactor: rearranging description for compactness

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,24 +1,17 @@
 <template>
-  <v-row
-    v-for="n in 4"
-    :key="n">
-    <v-col cols="6">
-      <v-card 
-      color="red"
-      class="pa-2 ma-2"
-      >
-        col 1
-      </v-card>
-    </v-col>
-    <v-col cols="3">
-      <v-card 
-      color="red"
-      class="pa-2 ma-2"
-      >
-        col 1
-      </v-card>
-    </v-col>
-  </v-row>
+<v-container  align="center">
+  <v-sheet  max-height="40%" max-width="40%" color="secondary">
+    <v-row justify="center"><v-card j color="primary">asdf</v-card></v-row>
+    <v-row v-for="n in 5" :key=n justify="center">{{ n }}
+      <v-col v-for="n in 3" :key=n cols="3">
+        <v-sheet color="primary" class="pa-2">
+          col {{ n }}
+        </v-sheet>
+      </v-col>
+    </v-row>
+
+  </v-sheet>
+</v-container>
 </template>
 
 <style>
