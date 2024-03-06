31ca1b661d5413999fc4bcac2fb54c369308ec9d
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2024-03-06 12:53:14 -0500

    feat: manage team view first commit

--- web/src/domains/teams/views/ManageTeamView.vue
+++ web/src/domains/teams/views/ManageTeamView.vue
@@ -1,88 +1,62 @@
 <template>
-    <h1>
-       Manage Team 
-       {{team.name}}
-    </h1>
-    <v-row>
-            <v-table>
-            <thead>
-                <tr class="text-left">
-                    <th>Name</th>
-                    <th>Day</th>
-                    <th>Time</th>
-                    <th>Length</th>
-                    <th>Signups</th>
-                    <th>Manage</th>
-                </tr>
-            </thead>
-                <tbody>
-                    <ManageShiftTableRow v-for="shift in shifts" :key="shift"
-                        :name="shift.name"
-                        :start="shift.start"
-                        :end="shift.end"
-                        :duration="shift.duration"
-                        :signups="shift.signups ?? 0"
-                        :capacity="shift.capacity"
-                        :id="shift.id"
-                        :day="shift.day"
-                        />
-                </tbody>
-        </v-table>
-        </v-row>
+    <v-container>
+        <h1>
+            Manage Team 
+            {{team.name}}
+        </h1>
+        <div>
+            <h1>{{ team.name }}</h1>
+            <p>{{ team.description }}</p>
+        </div>
+        <shift-table
+            @shift-action="load"
+            :shifts="shifts"
+            :forceManageButton="true"
+            :userShifts=[]
+        >
+        </shift-table>
+    </v-container>
 </template>
 <script>
 
 import { client } from '../../../../api-client/client'
+import ShiftTable from '../../shifts/components/ShiftTable.vue'
 
 export default {
     name: 'ManageTeamView',
     components: {
+        ShiftTable
         
-    },
-    props: {
-        teamId: {
-            type: String
-        }
     },
     data() {
         return {
+            teamId: this.$route.params.teamId,
             team: {},
             leads: [],
             shifts: [],
-            rows: 1
         }
     },
     methods: {
-        async getTeamById(teamId){
-            const team = await client.teams.getTeamById(teamId)
+        async getTeamShifts(){
+            let shifts = await client.teams.getShifts(this.teamId)
+            shifts = shifts.data
+            this.shifts = shifts
+        },
+
+        async getTeam(){
+            const team = await client.teams.getTeamById(this.teamId)
             this.team = team.data
         },
-        async getLeads(teamId){
-            const leads = await client.teams.getLeads(teamId)
-            this.leads = leads.data
-        },
-        async getShifts(teamId){
-            const shifts = await client.teams.getShifts(teamId)
-            this.shifts = shifts.data
-        },
         async load() {
-            await Promise.all([
-                await this.getTeamById(this.teamId),
-                await this.getLeads(this.teamId),
-                await this.getShifts(this.teamId)
-            ])
-        },
-        addRow() {
-            this.rows++
+            await this.getTeam()
+            await this.getTeamShifts()
         }
     },
-    computed:{
-    },
-    async created(){
+    async created() {
         await this.load()
     }
 }
 </script>
 <style>
     
-</style>../../../../api-client
+</style>
