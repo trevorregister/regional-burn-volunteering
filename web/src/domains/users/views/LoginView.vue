50b04e84795b8a632fbbf9657918811bfc2e5b5f
trevorregister <trevor.register@gmail.com>
2024-03-07 16:47:05 -0500

    clear default fill on login view

--- web/src/domains/users/views/LoginView.vue
+++ web/src/domains/users/views/LoginView.vue
@@ -37,7 +37,7 @@ export default {
   name: 'LoginView',
   data(){
     return {
-      email: 'lead87@email.com',
+      email: '',
       password: 'password',
       userStore: initUserStore()
     }
