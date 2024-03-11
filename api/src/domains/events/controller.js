ef623ff0cf05466616a8344f2fb9a1e7e490d53a
trevorregister <trevor.register@gmail.com>
2024-03-11 18:05:37 -0400

    feat: access control for events domain

--- api/src/domains/events/controller.js
+++ api/src/domains/events/controller.js
@@ -3,6 +3,10 @@ const {
     UpdateEvent, 
     GetEventById 
 } = require('./use-cases/_index')
+const {
+    IsEventLead
+} = require('../auth/use-cases/_index')
+
 const { HttpError } = require('../../config/errors')
 const EventDTO = require('./dto')
 
@@ -10,8 +14,12 @@ module.exports = (repository) => {
 
     const addEvent = async (req, res, next) => {
         try {
+            const isEventLeadCase = IsEventLead()
+            await isEventLeadCase.execute(req)
+
             const addEventCase = AddEvent(repository)
             const newEvent = await addEventCase.execute(EventDTO.toDb(req.body))
+
             res.status(201).send(EventDTO.toWeb(newEvent))
         } catch (err) {
             next(err)
@@ -20,11 +28,15 @@ module.exports = (repository) => {
 
     const getEventById = async (req, res, next) => {
         try {
+            const isEventLeadCase = IsEventLead()
+            await isEventLeadCase.execute(req)
+
             const getEventByIdCase = GetEventById(repository)
             const event = await getEventByIdCase.execute(req.params.id)
             if(!event){
                 throw new HttpError(404, `${id} event not found`)
             }
+
             res.status(200).send(EventDTO.toWeb(event))
         } catch (err) {
             next(err)
@@ -33,10 +45,14 @@ module.exports = (repository) => {
 
     const updateEvent = async (req, res, next) => {
         try {
+            const isEventLeadCase = IsEventLead()
+            await isEventLeadCase.execute(req)
+            
             const updateEventCase = UpdateEvent(repository)
             const { id } = req.params
             const update = req.body
             await updateEventCase.execute(id, update)
+            
             res.status(201).send('updated')
         } catch (err) {
             next(err)
