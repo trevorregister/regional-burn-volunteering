558449de0afc0d062b64b9733bf74ef2387dc2ce
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2024-03-11 20:44:23 -0400

    fixing cors

--- api/src/domains/users/controller.js
+++ api/src/domains/users/controller.js
@@ -64,7 +64,7 @@ module.exports = (repository) => {
             const verified = await loginUserCase.execute(email, password)
             verified
                 ? res.status(201)
-                     .cookie('authcookie', verified.token, {httpOnly: true, sameSite: 'strict'})
+                     .cookie('authcookie', verified.token, {httpOnly: true})
                      .send({user: UserDTO.toWeb(verified.user), token: verified.token})
                 : res.status(401).send(UserDTO.toWeb(verified))
         } catch (err) {
