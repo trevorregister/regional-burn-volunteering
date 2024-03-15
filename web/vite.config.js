aa69b80565a74d42e70ac204bc8e80d9a6ef1dd1
trevorregister <trevor.register@gmail.com>
2024-03-15 09:07:13 -0400

    api host update

--- web/vite.config.js
+++ web/vite.config.js
@@ -12,7 +12,7 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig(({mode}) =>{
   const API_HOST = (mode === 'development')
   ? "http://localhost:3000/api"
-  : "https://regional-burn-volunteering-api.onrender.com/api"
+  : "www.volunteer-app.api.trevorregister.com/api"
 
   return{
     plugins: [
