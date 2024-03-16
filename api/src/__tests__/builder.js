fc28450ea651e1ad50d8c0327744aeb199209d79
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2024-03-16 00:46:52 -0400

    feat: builder for unit tests

---
+++ api/src/__tests__/builder.js
@@ -0,0 +1,71 @@
+const { build, perBuild } = require('@jackfranklin/test-data-bot')
+const { faker } = require('@faker-js/faker')
+const bcrypt = require('bcrypt')
+const { ObjectId } = require('mongodb')
+const mongoose = require('mongoose')
+
+const UserDatabase = require ('../domains/users/data_access/database')
+const ShiftDatabase = require ('../domains/shifts/data_access/database')    
+const TeamDatabase = require ('../domains/teams/data_access/database')
+const EventDatabase = require ('../domains/events/data_access/database')
+const UserRepository = require('../domains/users/repository')
+const ShiftRepository = require('../domains/shifts/repository')
+const TeamRepository = require('../domains/teams/repository')
+const EventRepository = require('../domains/events/repository')
+
+const userDatabase = new UserDatabase()
+const shiftDatabase = new ShiftDatabase()
+const teamDatabase = new TeamDatabase()
+const eventDatabase = new EventDatabase()
+const userRepository = new UserRepository(userDatabase)
+const shiftRepository = new ShiftRepository(shiftDatabase)
+const teamRepository = new TeamRepository(teamDatabase)
+const eventRepository = new EventRepository(eventDatabase)
+
+
+faker.seed(123)
+
+const userBuilder = build({
+    name: 'User',
+    fields: {
+        _id: perBuild(() => new ObjectId()),
+        name: perBuild(() => faker.person.fullName()),
+        email: perBuild(() => faker.internet.email()),
+        hash: '$2b$10$0k888.GCzQolvjeD8JRntu8eYdi.GpGY0z3qytVal7HCTLNpTMJee',
+        role: 'user',
+        shifts: [],
+        events: [],
+        teams: []
+    },
+})
+
+class Builder {
+    constructor(){
+        this.userDb = userDatabase
+        this.teamDb = teamDatabase
+        this.shiftDb = shiftDatabase
+        this.eventDb = eventDatabase
+        this.userRepo = userRepository
+        this.teamRepo = teamRepository
+        this.shiftRepo = shiftRepository
+        this.eventRepo = eventRepository
+    }
+    randomId(){
+        return new mongoose.Types.ObjectId()
+    }
+    user(overrides = {}){
+        const user = userBuilder.one({overrides})
+        if(overrides.shifts){
+            user.shifts = [...overrides.shifts]
+        }
+        if(overrides.teams){
+            user.teams = [...overrides.teams]
+        }
+        return this.userDb.create(user)
+    }
+
+}
+
+module.exports = {
+    Builder
+}
---
+++ api/src/__tests__/users/LoginUser.test.js
@@ -0,0 +1,42 @@
+const { Builder } = require('../builder')
+const { LoginUser } = require('../../domains/users/use-cases/_index')
+const dotenv = require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
+const build = new Builder()
+
+describe('LoginUser', () => {
+
+    it('successful login should return user and token', async () => {
+        const user = await build.user()
+        const loginUserCase = LoginUser(build.userRepo)
+        const login = await loginUserCase.execute(user.email, 'password')
+        const verified = jwt.verify(login.token, process.env.JWT_SECRET)
+
+        expect(login.user.toObject()).toEqual({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            hash: user.hash,
+            __v: 0,
+            events: [],
+            shifts: user.shifts,
+            teams: user.teams,
+        })
+        expect(login.token).toBeDefined()
+        expect(verified.id).toEqual(user._id.toString())
+
+    })
+
+    it('unsuccessful login should return 401', async () => {
+        try{
+            const user = await build.user()
+            const loginUserCase = LoginUser(build.userRepo)
+            await loginUserCase.execute(user.email, 'asdf')
+        } catch(err){
+            expect(err.code).toBe(401)
+        }
+
+    })
+})
