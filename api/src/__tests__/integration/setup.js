e6154ee39205e50a8bc98285cc57b6a94207fd32
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2024-03-17 02:01:08 -0400

    formatting

--- api/src/__tests__/integration/setup.js
+++ api/src/__tests__/integration/setup.js
@@ -10,21 +10,39 @@ const routes = {
     events: '/api/events'
 }
 
-function requestsBuilder(request, domainRoute){
+function createRequestMethods(request, domainRoute){
     return {
-        post: (endpoint, data, token = '') => request.post(`${domainRoute}${endpoint}`).set({'Cookie': `authcookie=${token}`}).send(data),
-        get: (endpoint, token = '') => request.get(`${domainRoute}${endpoint}`).set({'Cookie': `authcookie=${token}`}),
-        patch: (endpoint, data, token = '') => request.put(`${domainRoute}${endpoint}`).set({'Cookie': `authcookie=${token}`}).send(data),
-        delete: (endpoint, token = '') => request.delete(`${domainRoute}${endpoint}`).set({'Cookie': `authcookie=${token}`})
+        post: (endpoint, data, token = '') => {
+            request
+                .post(`${domainRoute}${endpoint}`)
+                .set({'Cookie': `authcookie=${token}`})
+                .send(data)
+        },
+        get: (endpoint, token = '') => {
+            request
+                .get(`${domainRoute}${endpoint}`)
+                .set({'Cookie': `authcookie=${token}`})
+        },
+        patch: (endpoint, data, token = '') => {
+            request
+                .patch(`${domainRoute}${endpoint}`)
+                .set({'Cookie': `authcookie=${token}`})
+                .send(data)
+        },
+        delete: (endpoint, token = '') => {
+            request
+                .delete(`${domainRoute}${endpoint}`)
+                .set({'Cookie': `authcookie=${token}`})
+        }
     }
 }
 
 class Client {
     constructor(request, routes){
-        this.users = requestsBuilder(request, routes.users)
-        this.teams = requestsBuilder(request, routes.teams)
-        this.shifts = requestsBuilder(request, routes.shifts)
-        this.events = requestsBuilder(request, routes.events)
+        this.users = createRequestMethods(request, routes.users)
+        this.teams = createRequestMethods(request, routes.teams)
+        this.shifts = createRequestMethods(request, routes.shifts)
+        this.events = createRequestMethods(request, routes.events)
     }
 }
 
