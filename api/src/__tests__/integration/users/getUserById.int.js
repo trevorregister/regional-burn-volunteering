6da213f7cb59d96575cad1424479bbbedf4f3ed0
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2024-03-17 00:50:16 -0400

    getUserById int test

---
+++ api/src/__tests__/integration/users/getUserById.int.js
@@ -0,0 +1,30 @@
+const { Builder } = require('../../builder')
+const client = require('../setup')
+
+const builder = new Builder()
+
+describe('getUserById', () => {
+
+    it('given a request from an authenticated user, returns 403', async () =>{
+        try {
+            const user = await builder.user()
+            const loginRes = await client.users.post('login', {email: user.email, password: 'password'})
+            const { token } = loginRes.body
+            await client.users.get(`${user.id}`, token)
+        } catch (err) {
+            expect(err.code).toEqual(403)
+            
+        }
+    })
+    
+    it('given a request from an authenticated lead, returns user', async () =>{
+        const lead = await builder.user({role: 'lead'})
+        const user = await builder.user()
+        const loginRes = await client.users.post('login', {email: user.email, password: 'password'})
+        const { token } = loginRes.body
+        const res = await client.users.get(`${user.id}`, token)
+        expect(res.status).toBe(200)
+        expect(res.body.email).toBe(user.email)
+        
+    })
+})
