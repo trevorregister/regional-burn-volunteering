0cf13eb9d3cc83620db1e4bbcf78498b5ea78dc2
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2024-03-28 20:12:03 -0400

    feat: actual about page

--- web/src/domains/shared/AboutView.vue
+++ web/src/domains/shared/AboutView.vue
@@ -1,18 +1,19 @@
 <template>
   <v-container align="center">
-    <v-sheet class="fill-height" color="secondary">
-      <v-row justify="center">
-        <v-col class="fill-height">
-          <v-card color="primary"> asdf </v-card>
-        </v-col>
-      </v-row>
-      <v-row v-for="n in 5" :key="n" justify="center"
-        >{{ n }}
-        <v-col v-for="n in 3" :key="n" cols="3">
-          <v-sheet color="primary" class="pa-2"> col {{ n }} </v-sheet>
-        </v-col>
-      </v-row>
-    </v-sheet>
+    <h1>Hi, I'm Trevor</h1>
+    <p>I like Burning Man regional events.</p>
+    <p>
+      <a href="https://www.alchemyburn.com/" target="_blank">
+        (Alchemy is my favorite)
+      </a>
+    </p>
+    <p>I created this app as a personal project to build my web development skills.</p>
+    <p>But also in vague hopes that it might be useful to somebody one day.</p>
+    <p>
+      <a href="https://github.com/trevorregister/regional-burn-volunteering" target="_blank">
+        Check out the code on GitHub
+      </a>
+    </p>
   </v-container>
 </template>
 
